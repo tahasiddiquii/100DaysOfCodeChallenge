e3fb1d675a3815fe9935eed7ed140ac6eb889881
Taha <104767103+tahasiddiquii@users.noreply.github.com>
2023-07-19 15:22:23 +0530

    Create minmax.py

---
+++ Day 2: Lists and Array Manipulation/minmax.py
@@ -0,0 +1,7 @@
+my_list = [10, 5, 20, 8, 15]
+
+maximum_element = max(my_list)
+minimum_element = min(my_list)
+
+print("Maximum element:", maximum_element)
+print("Minimum element:", minimum_element)
